dd6e32ac39251dfa1a7e8a5c2a06f80d1a9a12b1
Zach Perrault <zach.perrault@gmail.com>
2015-11-03 08:27:59 -0500

    Paginates shared packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -157,7 +157,7 @@ class PackagesController < ApplicationController
   def index_shared
     @branches = PackageBranch.not_unit(current_unit).shared.paginate(
       :page => params[:page],
-      :per_page => 20
+      :per_page => params[:per_page] || 20
     ).includes(:shared_packages, :unit, :packages => [:require_items, :update_for_items]).find(:all, :joins => :packages, :order => 'packages.updated_at DESC')
   end
 
