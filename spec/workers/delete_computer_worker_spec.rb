60af8a3597e7738c5dd319ff96ff11696ca5471c
Zach Perrault <zach.perrault@gmail.com>
2015-11-05 13:43:59 -0500

    Add test for deleteing a computer that doesn't exist

--- spec/workers/delete_computer_worker_spec.rb
+++ spec/workers/delete_computer_worker_spec.rb
@@ -12,4 +12,14 @@ describe DeleteComputerWorker do
       Computer.find_by_id(computer_id).should == nil
     end
   end
+
+  context "when an nonexistant computer is passed" do
+    it "should be a noop" do
+      not_a_computer_id = -1
+      DeleteComputerWorker.perform_async(not_a_computer_id)
+      expect {
+        DeleteComputerWorker.drain
+      }.to_not raise_error
+    end
+  end
 end
