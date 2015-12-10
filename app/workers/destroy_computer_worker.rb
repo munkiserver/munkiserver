5181957fb9220a2d9666192e56a3da2254f595e0
Zach Perrault <zach.perrault@gmail.com>
2015-12-10 13:31:40 -0500

    Updating specs

--- app/workers/destroy_computer_worker.rb
+++ app/workers/destroy_computer_worker.rb
@@ -2,7 +2,7 @@ class DestroyComputerWorker < BaseWorker
   def perform(id)
 		with_connection do
 			# find_by_id returns `nil` if not found
-			computer = Computer.find_by_id(id)
+			computer = Computer.unscoped.find_by_id(id)
 			computer.destroy if computer
 		end
   end
