3f07bbdcc057a9c6c46a4ef4cd9f4e1cd071d7ad
Andy Van Fossen <vanfossa@ohio.edu>
2017-03-30 09:28:39 -0400

    Added 10.12.4 to the list of possible Mac OS versions

--- app/models/package.rb
+++ app/models/package.rb
@@ -65,7 +65,7 @@ class Package < ActiveRecord::Base
 
   FORM_OPTIONS = {:restart_actions         => [['None','None'],['Logout','RequireLogout'],['Restart','RequireRestart'],['Recommend Restart','RecommendRestart']],
                   :os_versions             => [[['Any','']], 
-                                                os_range(10,12,0..3),
+                                                os_range(10,12,0..4),
                                                 os_range(10,11,0..6),
                                                 os_range(10,10,0..5),
                                                 os_range(10,9,0..5),
