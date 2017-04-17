bd5ab5bc99b6cf63cd6f8c2a6c043be2adf28a59
Ricky Chilcott <ricky@rakefire.io>
2017-04-17 11:36:00 -0400

    Bump OS version numbers

--- app/models/package.rb
+++ app/models/package.rb
@@ -64,8 +64,8 @@ class Package < ActiveRecord::Base
   end
 
   FORM_OPTIONS = {:restart_actions         => [['None','None'],['Logout','RequireLogout'],['Restart','RequireRestart'],['Recommend Restart','RecommendRestart']],
-                  :os_versions             => [[['Any','']], 
-                                                os_range(10,12,0..3),
+                  :os_versions             => [[['Any','']],
+                                                os_range(10,12,0..5),
                                                 os_range(10,11,0..6),
                                                 os_range(10,10,0..5),
                                                 os_range(10,9,0..5),
